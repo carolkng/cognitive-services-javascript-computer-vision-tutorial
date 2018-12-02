a91bc0ca210b40ba878572c6edb895004c7e5104
Carol Ng <ng.km.carol@gmail.com>
2018-12-02 00:40:03 +0000

    📹🦃 Checkpoint ./common.js:2209287/114 ./mimic.html:2209287/7273 ./index.html:2209287/107 ./camera.js:2209287/11508 ./analyze.js:2209287/17175

--- analyze.js
+++ analyze.js
@@ -135,7 +135,90 @@ var common = (function()
     };
 })();
 
+function analyzeButtonClick() {
 
-// Initialize the JavaScript code.
-
-window.onload = common.init;
+    // Clear the display fields.
+    $("#sourceImage").attr("src", "#");
+    $("#responseTextArea").val("");
+    $("#captionSpan").text("");
+    
+    // Display the image.
+    var sourceImageUrl = $("#inputImage").val();
+    $("#sourceImage").attr("src", sourceImageUrl);
+    
+    AnalyzeImage(sourceImageUrl, $("#responseTextArea"), $("#captionSpan"));
+}
+/* Analyze the image at the specified URL by using Microsoft Cognitive Services Analyze Image API.
+ * @param {string} sourceImageUrl - The URL to the image to analyze.
+ * @param {<textarea> element} responseTextArea - The text area to display the JSON string returned
+ *                             from the REST API call, or to display the error message if there was 
+ *                             an error.
+ * @param {<span> element} captionSpan - The span to display the image caption.
+ */
+function AnalyzeImage(sourceImageUrl, responseTextArea, captionSpan) {
+    // Request parameters.
+    var params = {
+        "visualFeatures": "Categories,Description,Color",
+        "details": "",
+        "language": "en",
+    };
+    
+    var sub = document.getElementById("subscriptionKeyInput");
+    if (sub.value === "prefill-westus-cv") {
+      sub.key = process.env.WEST_US_CV_SUB_KEY;
+      sub.value = "westus";
+    } else if (sub.value === "prefill-canada-cv") {
+      sub.key = process.env.CANADA_CV_SUB_KEY;
+      sub.value = "canadacentral"
+    }
+  
+    // Perform the REST API call.
+    $.ajax({
+        url: common.uriBasePreRegion + 
+             sub.value + 
+             common.uriBasePostRegion + 
+             common.uriBaseAnalyze +
+             "?" + 
+             $.param(params),
+                    
+        // Request fheaders.
+        beforeSend: function(jqXHR){
+          jqXHR.setRequestHeader("Content-Type","application/json");
+          jqXHR.setRequestHeader("Ocp-Apim-Subscription-Key", 
+              encodeURIComponent(sub.value));
+        },
+        
+        type: "POST",
+        
+        // Request body.
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+    
+    .done(function(data) {
+        // Show formatted JSON on webpage.
+        responseTextArea.val(JSON.stringify(data, null, 2));
+        
+        // Extract and display the caption and confidence from the first caption in the description object.
+        if (data.description && data.description.captions) {
+            var caption = data.description.captions[0];
+            
+            if (caption.text && caption.confidence) {
+                captionSpan.text("Caption: " + caption.text +
+                    " (confidence: " + caption.confidence + ").");
+            }
+        }
+    })
+    
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        // Prepare the error string. 
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
+            jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+        
+        // Put the error JSON in the response textarea.
+        responseTextArea.val(JSON.stringify(jqXHR, null, 2));
+        
+        // Show the error message.
+        alert(errorString);
+    });
+}
